fabc7d84d97d27c39f4a43822c403ecc8d51584d
otmahlnheddouch <85652350+otmahlnheddouch@users.noreply.github.com>
2021-08-25 19:11:39 -0700

    Add files via upload

--- form-handler.php
+++ form-handler.php
@@ -8,12 +8,12 @@ $email_form = "otmanheddouchai@gmail.com"
 $email_subject = 'New Form Submission';
 
 $email_body = "User  Name : $name.\n".
-                "User email :$visitor_email.\n".
+                "User Email :$visitor_email.\n".
                 "Subject : $subject.\n".
-                "User message :$message .\n";
+                "User Message :$message .\n";
 $to = "otmanheddouchai@gmail.com"
-$headers = "From portfolio\r\n"
+$headers = "From: portfolio\r\n"
 $headers .= "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers)
-header("Location: index.html");
+header("Location: home.html");
 ?>
