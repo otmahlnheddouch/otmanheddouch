fc5cedc15a555a52756abc59d20e545d94c8e0a7
otmahlnheddouch <85652350+otmahlnheddouch@users.noreply.github.com>
2021-08-25 21:00:23 -0700

    Add files via upload

--- form.php
+++ form.php
@@ -1,8 +1,10 @@
-<?php     
-$to_email = 'portfoliootman@gmail.com';
-$subject = 'Testing PHP Mail';
-$message = 'This mail is sent using the PHP mail function';
-$headers = 'From: ';
-mail($to_email,$subject,$message,$headers);
-header("home.html")
+
+<?php
+$subject = $_POST["subject"];
+$name = $_POST["name"];
+$email = $_POST["email"];
+$msg = $_POST["message"];
+
+
+mail("otmanheddouchai@example.com",$subject,$msg);
 ?>
