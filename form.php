8126efc214c63cd273afd1f50e9be4c3af4a9ef1
otmahlnheddouch <85652350+otmahlnheddouch@users.noreply.github.com>
2021-08-25 21:24:52 -0700

    Update form.php

--- form.php
+++ form.php
@@ -4,8 +4,12 @@ $subject = $_POST["subject"];
 $name = $_POST["name"];
 $email = $_POST["email"];
 $msg = $_POST["message"];
+$emial_body = "User name:$name.\n".
+  "User  Email:$email.\n".
+  "Subject : $subject.\n".
+  "Message: $msg.\n";
 
 
-mail("otmanheddouchai@gmail.com",$subject,$msg);
+mail("otmanheddouchai@gmail.com",$emial_body);
 header("Location: home.html")
 ?>
