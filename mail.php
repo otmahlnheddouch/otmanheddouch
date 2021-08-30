a7ff7fec59f483059b5ad8a26a114246b32cdba9
otmahlnheddouch <85652350+otmahlnheddouch@users.noreply.github.com>
2021-08-30 18:09:32 +0100

    mail.php

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "otmanheddouch@gmail.com";
+$to = "otmanheddouchai@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@otmanheddouche.herokuapp.com" . "\r\n" ;
