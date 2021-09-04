4e94099d069413805a80bdb72f670e127853e11c
otmahlnheddouch <85652350+otmahlnheddouch@users.noreply.github.com>
2021-09-04 19:10:20 +0100

    mail.php

--- mail.php
+++ mail.php
@@ -1,12 +1,13 @@
 <?php
 //get data from form  
+
 $name = $_POST['name'];
 $email= $_POST['email'];
-$message= $_POST['message'];
+$message= $_POST['subject'];
 $to = "otmanheddouchai@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From:potfoliootman@gmail.com" . "\r\n" ;
+$headers = "From: noreply@otmanheddouch.herokuapp.com."\r\n";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
