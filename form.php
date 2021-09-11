12ddedfd0f089bb34e7864d60c7a825330aece9c
otmahlnheddouch <85652350+otmahlnheddouch@users.noreply.github.com>
2021-09-10 17:00:56 -0700

    Add files via upload

--- form.php
+++ form.php
@@ -1,17 +1,12 @@
 <?php
-$subject = $_POST["subject"];
-$name = $_POST["name"];
-$email = $_POST["email"];
-$msg = $_POST["message"];
-$emial_body = "User name:$name.\n".
-  "User  Email:$email.\n".
-  "Subject : $subject.\n".
-  "Message: $msg.\n";
-$email_from = "portfoliotman@gmail.com";
-$header = "From: $email_from \r\n";
-$header .= "Reply-To:$email \r\n";
-
-
-mail("otmanheddouchai@gmail.com","New form Submission",$emial_body,$header);
-header("Location: home.html")
+$to = 'otmanheddouchai@gmial.com'
+$subject = 'Mail From Website'
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+mail(
+  string $to,
+  string $subject,
+  string $message,
+): bool
 ?>
